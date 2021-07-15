23a593c6079c78750212a3ffe347333165076a9d
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2021-07-15 21:26:45 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# rahul-website
+create a website 
+1 page website 
+beautiful animations
---
+++ js/home.js
@@ -0,0 +1,5 @@
+//preloader.js
+var preloader=Document.getElementById("preloader")
+window.addEventListener("preloader",function(){
+    preloader.style.display="none";
+})
